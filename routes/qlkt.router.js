13a75c07782ad4833dab78627674ce06548eafe4
tktk <thangkhungtheki@gmail.com>
2023-12-07 11:19:32 +0700

    them file sript và req.fash thành công

--- routes/qlkt.router.js
+++ routes/qlkt.router.js
@@ -2,11 +2,14 @@ var express = require("express")
 var router = express.Router()
 var xulydb = require('../CRUD/xulydb')
 const ycsc = require('../CRUD/xulyyeucau')
+var mess = ""
 
 router.get('/', async (req, res) => {
   if (req.isAuthenticated()) {
     let user = await xulydb.timUser(req.user.username)
     // console.log(user)
+
+
     switch (user.role) {
       case "admin":
         res.redirect('/vattutest')
@@ -15,11 +18,14 @@ router.get('/', async (req, res) => {
         res.render('layoutkythuat/main/dashboard')
         break;
       case "nv":
-        res.render('layoutkythuat/user/dashboard', { data: user })
+        const successMessage = req.flash('success')[0];
+        res.render('layoutkythuat/user/dashboard', { data: user, successMessage })
         break;
       default:
         res.send('bạn đã Đăng ký thành công!!! <br\> Chào bạn: <b>' + user.username + ' </b>, vui lòng liên hệ admin và báo tên user, để được cấp quyền')
     }
+
+
 
   } else {
     res.redirect('/signin')
@@ -38,10 +44,14 @@ router.post('/taoyc', async (req, res) => {
       khancap: req.body.option,
       mota: req.body.areamota,
       trangthai: 'chờ duyệt',
-      
+
     }
     await ycsc.taoyc(doc)
+    // Lấy thông báo từ req.flash và truyền nó cho template
+
+    req.flash('success', 'Dữ liệu đã được lưu thành công!');
     res.redirect('/qlkt')
+
   }
 })
 
