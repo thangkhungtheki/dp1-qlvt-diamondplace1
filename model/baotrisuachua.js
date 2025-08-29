b9e0ed12754415dd672b0cb86c3a95b57f794d5b
thangkhungtheki <thangkhungtheki@gmail.com>
2025-08-29 11:34:33 +0700

    them modem sua chua

---
+++ model/baotrisuachua.js
@@ -0,0 +1,14 @@
+const mongooge = require("mongoose")
+const Schema = mongooge.Schema
+const schema = new Schema({
+    ngay: {type: Date, required: true},
+    idthietbi: {type: String, required: false},
+    phong: {type: String, required: false},
+    noidung: {type: String, required: false},
+    nguoithuchien: {type: String, required: false},
+    anh: {type: Array, required: false},
+    
+})
+
+
+module.exports = mongooge.model('baotrisuachua', schema, 'baotrisuachua')
